fee19253cb5bca643ad79b7256b1c473fe646704
Kihong Heo <kihong.heo@kaist.ac.kr>
2025-12-09 21:30:58 +0900

    [Simplify] add --all option

--- Source/DafnyCore/Options/SimplfyOptionBag.cs
+++ Source/DafnyCore/Options/SimplfyOptionBag.cs
@@ -11,10 +11,12 @@ using Microsoft.Dafny.Compilers;
 namespace Microsoft.Dafny;
 
 public class SimplifyOptionBag {
+  public static readonly Option<bool> All = new("--all", "Apply all transformations.") {};
   public static readonly Option<bool> NoAttribute = new("--no-attribute", "Remove attributes.") {};
   public static readonly Option<bool> ExplicitEmptyBlock = new("--explicit-empty-block", "Explicitly add {} for empty block.") {};
 
   static SimplifyOptionBag() {
+    OptionRegistry.RegisterOption(All, OptionScope.Cli);
     OptionRegistry.RegisterOption(NoAttribute, OptionScope.Cli);
     OptionRegistry.RegisterOption(ExplicitEmptyBlock, OptionScope.Cli);
   }
