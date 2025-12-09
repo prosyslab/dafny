fc22892713f04dc9a0827352bb5c4207da530e99
Kihong Heo <kihong.heo@kaist.ac.kr>
2025-12-09 22:32:23 +0900

    [Simplify] add --explicit-subseq (resolve #4)

--- Source/DafnyCore/Options/SimplfyOptionBag.cs
+++ Source/DafnyCore/Options/SimplfyOptionBag.cs
@@ -15,7 +15,8 @@ public class SimplifyOptionBag {
   public static readonly Option<bool> NoAttribute = new("--no-attribute", "Remove attributes.") {};
   public static readonly Option<bool> ExplicitEmptyBlock = new("--explicit-empty-block", "Explicitly add {} for empty block.") {};
   public static readonly Option<bool> ExplicitCardinality = new("--explicit-cardinality", "Explicitly add parentheses for cardinality expression.") {};
-  public static readonly Option<bool> ExplicitTypeArgs = new("--explicit-type-args", "Explicitly add parentheses for cardinality expression.") {};
+  public static readonly Option<bool> ExplicitTypeArgs = new("--explicit-type-args", "Explicitly add space for type arguments.") {};
+  public static readonly Option<bool> ExplicitSubseq = new("--explicit-subseq", "Explicitly add space for subsequence.") {};
 
   static SimplifyOptionBag() {
     OptionRegistry.RegisterOption(All, OptionScope.Cli);
@@ -23,6 +24,7 @@ public class SimplifyOptionBag {
     OptionRegistry.RegisterOption(ExplicitEmptyBlock, OptionScope.Cli);
     OptionRegistry.RegisterOption(ExplicitCardinality, OptionScope.Cli);
     OptionRegistry.RegisterOption(ExplicitTypeArgs, OptionScope.Cli);
+    OptionRegistry.RegisterOption(ExplicitSubseq, OptionScope.Cli);
   }
 }
 
