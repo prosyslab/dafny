3fb5822d9db7c42e2f5d01c5751d9bd74f2facb6
duncan020313 <milkbear0313@gmail.com>
2026-03-09 01:54:35 +0000

    [Trigger] fix quantifier scoping bug

--- Source/DafnyCore/Triggers/ExprSubstituter.cs
+++ Source/DafnyCore/Triggers/ExprSubstituter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
 
 namespace Microsoft.Dafny {
   public class ExprSubstituter : Substituter {
@@ -36,10 +37,26 @@ namespace Microsoft.Dafny {
         return ie;
       }
       if (expr is QuantifierExpr e) {
+        var inheritedUsedSubstMap = usedSubstMap;
+        usedSubstMap = [];
+
         var newAttrs = SubstAttributes(e.Attributes);
         var newRange = e.Range == null ? null : Substitute(e.Range);
         var newTerm = Substitute(e.Term);
         var newBounds = SubstituteBoundedPoolList(e.Bounds);
+        var localUsedSubstMap = usedSubstMap;
+        usedSubstMap = inheritedUsedSubstMap;
+
+        var substitutionsForCurrentQuantifier = localUsedSubstMap
+          .Where(entry => e.BoundVars.Exists(boundVar =>
+            FreeVariablesUtil.ContainsFreeVariable(entry.Item1, false, boundVar)))
+          .ToList();
+        var substitutionsForOuterQuantifiers = localUsedSubstMap
+          .Where(entry => !substitutionsForCurrentQuantifier.Exists(currentEntry =>
+            Triggers.ExprExtensions.ExpressionEq(currentEntry.Item1, entry.Item1)))
+          .ToList();
+        AddUniqueSubstitutions(usedSubstMap, substitutionsForOuterQuantifiers);
+
         if (newAttrs == e.Attributes && newRange == e.Range && newTerm == e.Term && newBounds == e.Bounds) {
           return e;
         }
@@ -53,7 +70,7 @@ namespace Microsoft.Dafny {
         }
 
         // conjoin all the new equalities to the range of the quantifier
-        foreach (var entry in usedSubstMap) {
+        foreach (var entry in substitutionsForCurrentQuantifier) {
           var eq = new BinaryExpr(e.Origin, BinaryExpr.ResolvedOpcode.EqCommon, entry.Item2, entry.Item1);
           newRange = newRange == null ? eq : new BinaryExpr(e.Origin, BinaryExpr.ResolvedOpcode.And, eq, newRange);
           newBoundVars.Add((BoundVar)entry.Item2.Var);
@@ -67,12 +84,23 @@ namespace Microsoft.Dafny {
           Contract.Assert(expr is ExistsExpr);
           newExpr = new ExistsExpr(e.Origin, newBoundVars, newRange, newTerm, newAttrs) { Bounds = newBounds };
         }
-        usedSubstMap.Clear();
 
         newExpr.Type = expr.Type;
         return newExpr;
       }
       return base.Substitute(expr);
+    }
+
+    private static void AddUniqueSubstitutions(
+      List<Tuple<Expression, IdentifierExpr>> target,
+      IEnumerable<Tuple<Expression, IdentifierExpr>> source) {
+      foreach (var entry in source) {
+        if (target.Exists(existing =>
+          Triggers.ExprExtensions.ExpressionEq(existing.Item1, entry.Item1))) {
+          continue;
+        }
+        target.Add(entry);
+      }
     }
   }
 }
